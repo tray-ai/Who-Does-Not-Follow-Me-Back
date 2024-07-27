73b79087c4a1af2b5b3dab5d1d8e93b60cf53881
deshaud <trayjobe1@gmail.com>
2024-07-27 14:27:44 -0500

    Update instagram_original.py
    July 27, 2024

--- instagram_original.py
+++ instagram_original.py
@@ -1,11 +1,11 @@
 import json
 from pathlib import Path
 
-# Read the files with users I follow, and user who do not follow back.
 following_path = Path('following.json')
 follower_path = Path('followers_1.json')
 
 try:
+    # Read the files with users I follow, and user who do not follow back.
     read_following_file = following_path.read_text()
     read_follower_file = follower_path.read_text()
 except FileNotFoundError:
@@ -21,13 +21,13 @@ followers = []
 # Loop through the users I follow to append them to the following list.
 for usernames in following_content['relationships_following']:
     names = usernames['string_list_data'][0]['value']
-    #add these users to the 'following' list.
+    # Add these users to the 'following' list.
     following.append(names)
 
 # Loop through user who follow me to append them to the followers list.
 for usernames in follower_content:
     names = usernames['string_list_data'][0]['value']
-    #add these users to the 'followers' list.
+    # Add these users to the 'followers' list.
     followers.append(names)
 
 # Compare lists for results of who does not follow me back.
